e0dc06e4f21fbd627631ec312c74da9acc7191cf
sb-exe <s.digitalmedium@gmail.com>
2024-11-04 23:38:18 -0500

    V2

--- src/controllers/leadController.js
+++ src/controllers/leadController.js
@@ -1,7 +1,6 @@
 const Lead = require('../models/Lead');
 const { validationResult } = require('express-validator');
-//comment for testing
-//testing
+
 exports.getLeads = async (req, res) => {
   try {
     const { status, source, search, sort = '-createdAt' } = req.query;
